41b693f8bb8ccb6d500f3cb9b2933c865d3950dc
gavlad8 <gavrila.nn.vladut@student.utcluj.ro>
2024-11-29 17:30:02 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,15 @@
+﻿
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        darkModeToggle.textContent = body.classList.contains('dark-mode')
+            ? 'Light Mode'
+            : 'Dark Mode';
+    });
+});
+    
